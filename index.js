a3c32cc339dd4e6ff4b9b8cd92b427f1f5c9eb19
AlexiyPok <41802113+AlexiyPok@users.noreply.github.com>
2021-10-01 14:52:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
-const router = require('./moduls/router')
+const router = require('./moduls/router');
 
-
+const fileupload = require('express-fileupload');
 
 const app = express();
 
 app.use(express.json());
 app.use("/", router);
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'static'))); // Load frontPage 'index.html'
 
 const start = () => {
     try {
